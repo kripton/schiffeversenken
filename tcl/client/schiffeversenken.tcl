8b98251adebd5fc0b3757218db9bbb5c04396620
Jannis Achstetter <kripton@kripserver.net>
2014-03-09 00:34:59 +0100

    Continue the tcl client

--- tcl/client/schiffeversenken.tcl
+++ tcl/client/schiffeversenken.tcl
@@ -12,7 +12,11 @@ if {0} {
 }
 
 wm title . "Schiffe versenken"
-wm geometry . =420x350
+wm geometry . =550x370
+
+set serverip "127.0.0.1"
+set playername "Spieler"
+set connected 0
 
 image create photo wasser -file "wasser.gif"
 image create photo wasser-hit -file "wasser-hit.gif"
@@ -24,9 +28,12 @@ labelframe .eigenes -text "Eigenes Spielfeld"
 labelframe .fremdes -text "Gegnerisches Spielfeld"
 labelframe .gamechat -text "Chat des Spielraums"
 labelframe .mainchat -text "Globaler Chatraum"
+labelframe .settings -text "Einstellungen"
+labelframe .connectbox -text "Serververbindung"
 
 grid .eigenes .fremdes -sticky nsew
 grid .gamechat .mainchat -sticky nsew
+grid .settings .connectbox -sticky nsew
 
 for {set i 97} {$i < 107} {incr i} {
   for {set j 0} {$j < 10} {incr j} {
@@ -57,3 +64,56 @@ listbox .mainchat.list -width 27 -height 5
 entry .mainchat.entry -width 27
 grid .mainchat.list
 grid .mainchat.entry
+
+label .settings.label -text "Spielername:"
+entry .settings.name -textvariable playername
+grid .settings.label .settings.name
+
+label .connectbox.label -text "Server:"
+entry .connectbox.entry -textvariable serverip
+button .connectbox.button -text "Verbinden" -command connect
+grid .connectbox.label .connectbox.entry .connectbox.button
+
+proc disconnect {} {
+  global sock connected
+
+  catch {
+    close $sock
+  }
+  .connectbox.button configure -text "Verbinden" -command connect
+  set $connected 0
+}
+
+proc connect {} {
+  global sock serverip
+
+  set sock [socket $serverip 9034]
+  fileevent $sock readable [list parseData $sock]
+}
+
+proc parseData {sock} {
+  global connected playername
+
+  set indata [gets $sock]
+  puts "GOT $indata COMMAND [lindex [split $indata] 0]"
+
+  switch [lindex [split $indata] 0] {
+    "101" { # Anfrage Spielername
+      puts $sock "210 $playername"
+    }
+
+    "305" { # Name schon vergeben
+      disconnect
+    }
+
+    "601" { # willkommen
+      set $connected 1
+      .connectbox.button configure -text "Trennen" -command disconnect
+    }
+    "603" { # ping
+      puts $sock "604 -- bin noch da"
+    }
+  }
+  flush $sock
+
+}
