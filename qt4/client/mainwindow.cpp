f4f237beae1b54259c72f33670ac5f8b7075833d
Jannis Achstetter <kripton@kripserver.net>
2014-03-13 01:29:06 +0100

    - SIGSEGV beim Start des Clients behoben - Hauptfenster erweitert

--- qt4/client/mainwindow.cpp
+++ qt4/client/mainwindow.cpp
@@ -2,9 +2,10 @@
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
-    ui->lineEdit_port->setValidator(new QIntValidator(1, 65535, this));
-    con = new Connection(this);
     ui->setupUi(this);
+
+    ui->lineEdit_port->setValidator(new QIntValidator(1, 65535, this));
+    ui->lineEdit_name->setText(QString("Player%1").arg(qrand()));
 }
 
 MainWindow::~MainWindow()
